0b6b2f45c012667eaa7b5efbdbe95868eba1b2fc
James McGuinness <jrm@yelp.com>
2014-07-31 19:42:27 -0700

    LE BUMP

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.3.2',
+    version='0.3.3',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         'kazoo==1.3.1',
         'PyYAML==3.10',
-        'service-deployment-tools==0.1.28',
+        'service-deployment-tools==0.1.30',
     ],
     entry_points={
         'console_scripts': [
