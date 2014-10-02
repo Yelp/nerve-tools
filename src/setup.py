35f08fa58b9ce59ee6c11fd24a479e10817b977d
Josh Snyder <josnyder@yelp.com>
2014-10-01 21:15:52 -0700

    bump service_deployment_tools

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.4.3',
+    version='0.4.4',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         'kazoo==1.3.1',
         'PyYAML==3.10',
-        'service-deployment-tools==0.3.2',
+        'service-deployment-tools==0.4.3',
     ],
     entry_points={
         'console_scripts': [
