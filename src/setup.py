6184ee7f58c946c5370863621c22b5f8a2efa7db
Joseph Lynch <jlynch@yelp.com>
2015-04-01 18:17:22 -0700

    Bump to v0.7.20

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.7.19',
+    version='0.7.20',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
