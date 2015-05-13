d908b943ff7d977303753716cba4a2f394c25e26
Tomas Doran <tdoran@yelp.com>
2015-05-13 05:14:58 -0700

    Update environment_tools to 1.0.3

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.8.8',
+    version='0.8.9',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
