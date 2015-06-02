f4c536e4a43cc0ff020e7c23c55555f746e28c66
Keshav Varma <keshav@yelp.com>
2015-06-02 15:29:13 -0700

    Bump to 0.8.14

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.8.13',
+    version='0.8.14',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
