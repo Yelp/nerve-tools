75212cdbc06b20b9c3bde83f64499f8bbfc28559
Keshav Varma <keshav@yelp.com>
2015-06-19 17:30:49 -0700

    Bump paasta_tools version to 0.11.20

--- src/setup.py
+++ src/setup.py
@@ -18,7 +18,7 @@ setup(
         'environment_tools>=1.0.1',
         'kazoo>=2.0.0',
         'PyYAML>=3.11',
-        'paasta-tools==0.9.61',
+        'paasta-tools==0.11.20',
         'service-configuration-lib==0.9.2',
     ],
     entry_points={
