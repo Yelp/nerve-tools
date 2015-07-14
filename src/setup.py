bdcfa1d3bc83f2cfabfb6ed276599b2b629f9ee1
Tomas Doran <tdoran@yelp.com>
2015-07-14 05:54:45 -0700

    Bump version to 0.8.18

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.8.17',
+    version='0.8.18',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
