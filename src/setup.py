36f650cd1d0168d35460f34903d271222b452fa7
Keshav Varma <keshav@yelp.com>
2015-08-21 10:57:19 -0700

    Release nerve_tools 0.8.25

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.8.24',
+    version='0.8.25',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
