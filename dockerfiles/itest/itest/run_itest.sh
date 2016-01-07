283c32fac70c118491463e0134ff25d6b1bd90d2
Joseph Lynch <joe.e.lynch@gmail.com>
2016-01-07 14:02:39 -0800

    Properly use shlibs for dependency mgmt

--- dockerfiles/itest/itest/run_itest.sh
+++ dockerfiles/itest/itest/run_itest.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-set -e
-
 echo "Installing nerve-tools package"
 dpkg -i /work/dist/nerve-tools_*.deb
+
+# Set -e here because the previous install should fail lacking dependencies
+# and then we can fix it here
+set -e
+apt-get -y install -f
 
 echo "Testing that pyyaml uses optimized cyaml parsers if present"
 /usr/share/python/nerve-tools/bin/python -c 'import yaml; assert yaml.__with_libyaml__'
