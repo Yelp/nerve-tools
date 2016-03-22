a06669b7b17404e7eaa23e69dbe3de02ef0665f9
Federico Giraud <fgiraud@yelp.com>
2016-03-22 08:17:56 -0700

    Bump version to 0.10.3

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.10.2',
+    version='0.10.3',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
