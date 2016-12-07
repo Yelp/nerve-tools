0126e09274649896b50bee8512e87e97e903846f
Matthew Smith <matts@yelp.com>
2016-12-07 13:35:42 -0800

    bumped version to 0.13.2

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.13.1',
+    version='0.13.2',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
