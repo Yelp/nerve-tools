8d9938d1aef688c57734753b7f11ad54f82156b4
Joseph Lynch <jlynch@yelp.com>
2017-04-17 11:50:41 -0700

    Release 0.14.0

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.13.6',
+    version='0.14.0',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
