3b85ca8b571cf91c1891d3d87ca8e0533f31e0f6
Matthew Mead-Briggs <mmb@yelp.com>
2018-06-21 03:39:08 -0700

    Bump version

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.14.5',
+    version='0.15.0',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
