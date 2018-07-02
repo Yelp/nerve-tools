50af61a9706b5d6ca44656f84d85582b938e4d77
gabrielv <gabrielv@yelp.com>
2018-07-02 09:53:29 -0700

    Bump version

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerve-tools',
-    version='0.15.2',
+    version='0.15.3',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
