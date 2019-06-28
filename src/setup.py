7eca88cca6218fe4a5374f512f00d02adad5b456
Kaisen Chen <kaisen@yelp.com>
2019-06-28 11:11:33 -0700

    Released 0.16.0 via make release

--- src/setup.py
+++ src/setup.py
@@ -14,7 +14,7 @@ def get_install_requires():
 
 setup(
     name='nerve-tools',
-    version='0.15.12',
+    version='0.16.0',
     provides=['nerve_tools'],
     author='John Billings',
     author_email='billings@yelp.com',
