c6fe4e260f23c47107b19cfb1188604b8a47cb2b
Maksym Melnychok <maksym@yelp.com>
2021-05-14 05:49:28 -0700

    Released 1.1.0 via make release

--- src/setup.py
+++ src/setup.py
@@ -14,7 +14,7 @@ def get_install_requires():
 
 setup(
     name='nerve-tools',
-    version='1.0.1',
+    version='1.1.0',
     provides=['nerve_tools'],
     author='Yelp',
     author_email='compute-infra@yelp.com',
