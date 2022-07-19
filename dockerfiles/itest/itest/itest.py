6e29da8fa9753b33bf4deb37ea46e985c0462855
Jens Langhammer <langj@yelp.com>
2022-07-19 08:32:18 -0700

    add basic CI

--- dockerfiles/itest/itest/itest.py
+++ dockerfiles/itest/itest/itest.py
@@ -127,7 +127,7 @@ def test_nerve_service_config(setup):
                 "uri": "/http/service_three.main/1024/status",
                 "headers": {
                     "Host": "www.test.com",
-                    "X-yelp-expected-service": "service_three.main",
+                    "X-smartstack-expected-service": "service_three.main",
                 },
                 "expect": "OK",
             },
