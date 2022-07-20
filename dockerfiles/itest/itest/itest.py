cc2ce6ce0e48477f7099bfc104815a0b28d51293
Jens Langhammer <langj@yelp.com>
2022-07-20 05:52:04 -0700

    lowercase headers

--- dockerfiles/itest/itest/itest.py
+++ dockerfiles/itest/itest/itest.py
@@ -127,7 +127,7 @@ def test_nerve_service_config(setup):
                 "uri": "/http/service_three.main/1024/status",
                 "headers": {
                     "Host": "www.test.com",
-                    "X-smartstack-expected-service": "service_three.main",
+                    "x-smartstack-expected-service": "service_three.main",
                 },
                 "expect": "OK",
             },
