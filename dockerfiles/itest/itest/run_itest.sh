073c3aab29cb012e63d3a5fd0fffe552c798261e
Evan Krall <krall@yelp.com>
2022-09-02 18:29:34 -0700

    Make integration tests download paasta-tools debs from github, so that we can use paasta_dump_locally_running_services

--- dockerfiles/itest/itest/run_itest.sh
+++ dockerfiles/itest/itest/run_itest.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
-echo "Installing nerve-tools package"
-dpkg -i /work/dist/nerve-tools_*.deb
-
-# Set -e here because the previous install should fail lacking dependencies
-# and then we can fix it here
+set -vx
 set -e
+
+echo "installing paasta-tools (dependency of nerve-tools.)"
+. /etc/lsb-release
+PAASTA_VERSION=0.144.7
+PAASTA_DEB_NAME=paasta-tools_${PAASTA_VERSION}.${DISTRIB_CODENAME}1_amd64.deb
+wget "https://github.com/Yelp/paasta/releases/download/v${PAASTA_VERSION}/${PAASTA_DEB_NAME}"
+
+# We use || true here because any missing dependencies will cause dpkg -i to return an error.
+# We fix the missing dependencies later.
+dpkg -i "./${PAASTA_DEB_NAME}" || true
+
+echo "Installing nerve-tools package"
+dpkg -i /work/dist/nerve-tools_*.deb || true
+
+echo "Fixing missing dependencies"
 apt-get -y install -f
 
 echo "Testing that pyyaml uses optimized cyaml parsers if present"
