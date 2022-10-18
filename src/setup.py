426035cf95f1b60197c18d5e6c82a7292a42bb7b
Jens Langhammer <langj@yelp.com>
2022-10-18 06:08:11 -0700

    Released 2.2.2 via make release

--- src/setup.py
+++ src/setup.py
@@ -14,7 +14,7 @@ def get_install_requires():
 
 setup(
     name='nerve-tools',
-    version='2.2.1',
+    version='2.2.2',
     provides=['nerve_tools'],
     author='Yelp',
     author_email='compute-infra@yelp.com',
